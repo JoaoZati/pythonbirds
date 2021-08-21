acb54c32a0f692822c245eb66b29dd9ecaa8b009
JoaoZati <joaozati@gmail.com>
2021-08-21 19:17:20 -0300

    Criado instancias de dados, idade e nome

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,16 @@
 class Pessoa:
-    pass
+    def __init__(self, nome, idade):
+        self.idade = idade
+        self.nome = nome #None para nada
+
+    def comprimentar(self):
+        return f'olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa('Luciano')
+    print(Pessoa.comprimentar(p))
+    print(id(p))
+    print(p.comprimentar())
+    print(p.nome)
+    p.nome = "Renzo"
+    print(p.nome)
